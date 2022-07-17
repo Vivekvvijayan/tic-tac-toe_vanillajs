ec25e87391d0719439dda48b122192a00a185a63
Vivekvvijayan <71512595+Vivekvvijayan@users.noreply.github.com>
2022-07-17 18:18:47 +0530

    audio  an hover

--- functions/Tones.js
+++ functions/Tones.js
@@ -1,7 +1,7 @@
 const Tones = {
     WIN:'https://github.com/Vivekvvijayan/tic-tac-toe_vanillajs/blob/master/tones/WIN.wav?raw=true',
 
-    DRAW:'https://github.com/Vivekvvijayan/tic-tac-toe_vanillajs/blob/master/tones/DRAW.mp3?raw=true',
-    X_MOVE:'https://github.com/Vivekvvijayan/tic-tac-toe_vanillajs/blob/master/tones/X.mp3?raw=true',
-    O_MOVE:'https://github.com/Vivekvvijayan/tic-tac-toe_vanillajs/blob/master/tones/O.mp3?raw=true'
+    DRAW:"https://github.com/Vivekvvijayan/tic-tac-toe_vanillajs/blob/master/tones/DRAW.mp3?raw=true",
+    X_MOVE:"https://github.com/Vivekvvijayan/tic-tac-toe_vanillajs/blob/master/tones/X.mp3?raw=true",
+    O_MOVE:"https://github.com/Vivekvvijayan/tic-tac-toe_vanillajs/blob/master/tones/O.mp3?raw=true"
 }
