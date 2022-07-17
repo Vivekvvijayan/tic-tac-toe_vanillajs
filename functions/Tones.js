abfbd75fa2c7adfdecd2ed066e88b035d1b50632
Vivekvvijayan <71512595+Vivekvvijayan@users.noreply.github.com>
2022-07-17 18:09:38 +0530

    audio

--- functions/Tones.js
+++ functions/Tones.js
@@ -1,5 +1,13 @@
+// const Tones = {
+//     WIN:'../tones/WIN.wav',
+//     DRAW:'../tones/DRAW.mp3',
+//     X_MOVE:'../tones/X.mp3',
+//     O_MOVE:'../tones/O.mp3'
+// }
+
 const Tones = {
-    WIN:'../tones/WIN.wav',
+    WIN:'https://github.com/Vivekvvijayan/tic-tac-toe_vanillajs/blob/master/tones/WIN.wav?raw=true',
+    
     DRAW:'../tones/DRAW.mp3',
     X_MOVE:'../tones/X.mp3',
     O_MOVE:'../tones/O.mp3'
