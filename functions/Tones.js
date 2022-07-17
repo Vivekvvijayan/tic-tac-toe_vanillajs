38f7aefeef92f27cc5c0ef7fd5c5f845e9b54eb7
Vivekvvijayan <71512595+Vivekvvijayan@users.noreply.github.com>
2022-07-17 17:38:11 +0530

    audio probelm solved

--- functions/Tones.js
+++ functions/Tones.js
@@ -1,6 +1,6 @@
 const Tones = {
-    WIN:'https://vivekvvijayan.github.io/WIN.wav',
+    WIN:'https://vivekvvijayan.github.io/tones/WIN.wav',
     DRAW:'https://vivekvvijayan.github.io/tones/DRAW.mp3',
-    X_MOVE:'https://vivekvvijayan.github.io/Tones/X.mp3',
-    O_MOVE:'https://vivekvvijayan.github.io/Tones/O.mp3'
+    X_MOVE:'https://vivekvvijayan.github.io/tones/X.mp3',
+    O_MOVE:'https://vivekvvijayan.github.io/tones/O.mp3'
 }
